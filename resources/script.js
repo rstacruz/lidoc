52e4e41ecbe5ebdeda556e47c90c0e421ddf81a0
Rico Sta. Cruz <rico@ambiescent.com>
2012-08-19 06:34:59 +0800

    Keep the files tab open.

--- resources/script.js
+++ resources/script.js
@@ -1,13 +1,22 @@
 (function() {
 
-  $('#sidebar .tabs button').on('click', function() {
-    var href = $(this).attr('data-tab');
-
+  function openSidebarTab(href) {
     $('#sidebar .tabs button').removeClass('active');
     $('#sidebar .tabs button[data-tab="'+href+'"]').addClass('active');
 
     $('#sidebar .list').hide();
     $('#sidebar .list.'+href).show();
+  }
+
+  $('#sidebar .tabs button').on('click', function() {
+    var href = $(this).attr('data-tab');
+    openSidebarTab(href);
+  });
+
+  $(function() {
+    if (window.location.hash === '#_files') {
+      openSidebarTab('files');
+    }
   });
 
 })();
